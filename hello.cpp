228e1803ba3c71bb033614b86d733beda96111fa
bmilk23 <bernardo.costa.leite.23@gmail.com>
2025-02-15 16:29:50 +0000

    Second step

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
 
     cout<<"Hello World"<< endl;
+
+    for (int i = 0; i<=16; i++){
+        cout<<i<<dec<<"\t"<<i<<oct<<"\t"<<i<<hex<<"\t"<<endl;
+    }
+    cout<<"\n";
+    
+    for (int i = 0; i<=16; i++){
+        cout<<dec<<i<<"\t"<<oct<<i<<"\t"<<hex<<i<<"\t"<<endl;
+    }
+
     return 0;
 };
