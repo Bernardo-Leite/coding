fb5abc7e1c838aba7b99dbb14347d5862f0f3d11
bmilk23 <bernardo.costa.leite.23@gmail.com>
2025-02-15 19:04:54 +0000

    Another commit

--- hello.cpp
+++ hello.cpp
@@ -3,17 +3,20 @@
 using namespace std;
 
 int main(){
-
+    int i;
     cout<<"Hello World"<< endl;
 
     for (int i = 0; i<=16; i++){
         cout<<i<<dec<<"\t"<<i<<oct<<"\t"<<i<<hex<<"\t"<<endl;
-    }
+    };
     cout<<"\n";
     
     for (int i = 0; i<=16; i++){
         cout<<dec<<i<<"\t"<<oct<<i<<"\t"<<hex<<i<<"\t"<<endl;
-    }
+    };
+
+    cout<<i++<<endl;
+    cout<<i<<endl;
 
     return 0;
 };
